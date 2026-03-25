fc950ac51a24ad894f61890d9801e00271523564
Thapelo Moshoadiba <54323840+ThapeloMoshoadiba@users.noreply.github.com>
2026-03-25 12:32:31 +0200

    update-transactions-integration (#2)
    Co-authored-by: Thapelo <thapelomoshoadiba@capitecbank.co.za>

--- target/generated-sources/annotations/com/capsule/corp/domain/mapper/ClientMapperImpl.java
+++ target/generated-sources/annotations/com/capsule/corp/domain/mapper/ClientMapperImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-03-25T01:54:20+0200",
+    date = "2026-03-25T12:30:28+0200",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.3 (Amazon.com Inc.)"
 )
 @Component
